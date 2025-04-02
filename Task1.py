887bd80d8716b7d8d2f4b15c8991c7353fa683b1
saumya-736 <144018393+saumya-736@users.noreply.github.com>
2025-04-02 22:13:50 +0530

    Add files via upload
    Added Python scripts for Task 1 and Task 2"

---
+++ Task1.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the First Number :"))
+num2 = float(input("Enter the Second Number :"))
+print(f"Addition : {num1 + num2}")
+print(f"Subtraction : {num1 - num2}")
+print(f"Multiplication  : {num1 * num2}")
+print(f"Division : {num1 / num2}")
---
+++ Task2.py
@@ -0,0 +1,3 @@
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+print(f"Hello, {fname} {lname}! Welcome to Python programming.")
